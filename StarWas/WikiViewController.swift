d83c79472c18be50367320d6ca862d36c42a4492
icapa <ivan.olea@gmail.com>
2016-06-28 23:00:33 +0200

    wikiVC initial commit

---
+++ StarWas/WikiViewController.swift
@@ -0,0 +1,26 @@
+//
+//  WikiViewController.swift
+//  StarWas
+//
+//  Created by Iván Cayón Palacio on 28/6/16.
+//  Copyright © 2016 KeepCoding. All rights reserved.
+//
+
+import UIKit
+
+class WikiViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+   
+}
