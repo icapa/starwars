e62cbf106f4a6c433db34a4c18df31ac17a693eb
icapa <ivan.olea@gmail.com>
2016-06-28 22:20:23 +0200

    Creada la imagen y botones, sincronizamos el modelo

--- StarWas/CharacterViewController.swift
+++ StarWas/CharacterViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 class CharacterViewController: UIViewController {
+    //MARK: - Properties
     let model : StarWarsCharacter
     
+    @IBOutlet weak var photoView: UIImageView!
+    
+    //MARK: - Initialization
     init(model: StarWarsCharacter){
         self.model = model
         super.init(nibName: "CharacterViewController", bundle: nil)
@@ -19,5 +23,48 @@ class CharacterViewController: UIViewController {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    func syncModelWithView(){
+        //Photo
+        photoView.image = model.photo
+        
+        //Alias
+        title = model.name
+        
+    }
+    
+    //MARK: - Actions
+    @IBAction func playSound(sender: AnyObject) {
+    }
+    @IBAction func displayWiki(sender: AnyObject) {
+    }
 
+    
+    // should -- will -- did
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Antes de que las vista tenga sus dimensiones
+        // Una sola vez
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        // Para que el VC se ajuste al espacio que deja el navigation
+        //edgesForExtendedLayout = .None
+        
+        // Justo antes de mostrarse (después de viewDidLoad)
+        // Posiblemente más de una vez
+        syncModelWithView()
+        
+    }
+
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+    
 }
